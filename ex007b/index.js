39c52b3846a3cdce1c2b18fee2b94e9590da8176
gustavoesposito <espositogustavo796@gmail.com>
2022-05-26 12:26:42 -0300

    att

---
+++ ex007b/index.js
@@ -0,0 +1,4 @@
+var vel = '90km'
+console.log (` A Velocidade do seu carro é de ${vel}`)
+
+console.log('Dirija sempre com segurança, utlize cinto de sgeurança e sempre dirija ate o limite de velociade permitido ')
