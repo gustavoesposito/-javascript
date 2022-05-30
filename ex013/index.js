fbec05767ccb4f1e4c41636fa071ed5a11cae8d1
gustavoesposito <espositogustavo796@gmail.com>
2022-05-30 12:31:46 -0300

    array

---
+++ ex013/index.js
@@ -0,0 +1,16 @@
+var num= [5,8,2,9,3]
+ num.sort() 
+ num.push(17)
+console.log(`Nossos números são ${num}`)
+num[3]=6
+ console.log(`O vetor  tem ${num.length} poisoções`)
+ let valores =[5,6,7,3,4]
+ valores.sort
+for(let pos=0; pos<num.length; pos++){
+console.log(`A posição ${pos} e tem valor ${valores[pos]} `)
+}
+for(let pos in valores){
+    console.log(`A posição ${pos} tem valores ${
+valores[pos]
+    }`)
+}
