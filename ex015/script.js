e7af615bb9011c30afcb0d3f89351543fe128a18
gustavoesposito <espositogustavo796@gmail.com>
2022-06-01 08:51:42 -0300

    att com erro  :(

--- ex015/script.js
+++ ex015/script.js
@@ -2,6 +2,8 @@ let num = document.querySelector('input#fnum')
 let lista = document.querySelector('select#flista')
 let res = document.querySelector('div#res')
 let valores = []
+
+ 
  
 function isNumero(n){
     if(Number(n) >= 1 && Number(n) <=100 ) {
@@ -21,8 +23,39 @@ return false
 
 function adicionar(){
     if(isNumero (num.value) && !inLista(num.value, valores)) {
-  window.alert('Tudo ok')
+ valores.push(Number(num.value))
+ let item = document.createElement('option')
+ item.text = `Valor ${num.value} adicionado`
+ lista.appendChild(item)
+ res.innerHTML= ''
     } else {
         window.alert('Valor inválid ou já encontrado na lista. ')
+    }
+    num.value= ''
+    num.focus()
+}
+function finalizar(){
+    if (valores.length == 0){
+        window.alert('Adicicone valores antes de finalilzar')
+    }else{
+let tot = valores.length
+let maior= valores [0]
+let menor = valores[0]
+let soma = 0
+let media =0
+for (let pos in valores ){
+    soma += valores[pos]
+    if ( valores [pos] > maior)
+    maior = valores [pos]
+    if (valores[pos ] < menor)
+    menor = valores[pos]
+}
+media = soma / tot
+        res.innerHTML=''
+      res.innerHTML +=   `<p> Ao todo fora adicionados ${ tot} números.</p>`
+      res.innerHTML +=`<p> O maior número adicionado foi ${maior }</p> `
+      res.innerHTML +=`<p> A soma do snúmeor s foi ${soma}.</p>`
+      res.innerHTML +=`<p> A média dos números foi ${media}.</p>`
+
     }
 }
