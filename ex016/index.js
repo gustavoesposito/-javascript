4dc79d6fa324ad5aecf875b6a5da16460f4e2383
gustavoesposito <espositogustavo796@gmail.com>
2022-06-02 10:27:35 -0300

    att

---
+++ ex016/index.js
@@ -0,0 +1,32 @@
+ /*let pessoa   ={
+     nome: ' Gustavo',
+     sobrenome: ' Esposito',
+     estaaprovado:true,
+     altura: ' 1.72',
+     peso: '66.00',
+     idade : '17'
+ }
+ console.log(peso)*/
+ // função 
+ /* let peso = '66.0'
+ function alterarPeso(kg){
+peso = kg
+ }
+ console.log(peso)
+ alterarPeso('64.00')
+ console.log(peso)*/
+ let a = 'vermelho'
+ let b = 'azul  '
+  function trocarCora(b){
+a = b
+ 
+  }
+  function trocarCorb(a){
+      b=a
+  }
+  console.log(a)
+  console.log(b)
+  trocarCora('azul')
+  trocarCorb('vermelho')
+  console.log(a)
+  console.log(b)
