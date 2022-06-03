aea91a7160b9359bf13fb07356708f774e719dfa
gustavoesposito <espositogustavo796@gmail.com>
2022-06-03 18:39:23 -0300

    att

--- ex034/index.js
+++ ex034/index.js
@@ -1,4 +1,32 @@
-//Montando endereço
- function montandoEdereço(){
-
+//Objeto Endereço
+//priemiro jeito
+let endereço={
+    Rua:'Emílio Cardoso Aires',
+    Cidade :'Emílio Cardoso Aires',
+    CEP: 08090350
+}
+ function exibirEndereço(Rua, Cidade, CEP){
+  return{
+        Rua ,
+        Cidade ,
+        CEP 
+  
  }
+ }
+ let endereço1 = exibirEndereço('jose amilton nao sei oque', 'São Paulo', 08070340)
+ console.log(endereço1)
+ 
+ //Outro jeito
+ //melhor jeito
+
+ let endereco = {
+     rua:'a',
+     cidade : 'b',
+     cep:'c'
+ }
+  function exibirEndereco(endereco){
+      for (let chave in endereco)
+      console.log(chave, endereco[chave])
+  }
+  exibirEndereco(endereco)
+ 
