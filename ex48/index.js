5d9b480d19495e3bb2cdfff95bd240007281379c
gustavoesposito <espositogustavo796@gmail.com>
2022-06-04 11:15:09 -0300

    att

---
+++ ex48/index.js
@@ -0,0 +1,13 @@
+//***COMBINANDO ARRAYS COM JOIN ***
+
+const numeros=[1,2,3,4,5,6]
+const combinando = numeros.join('-')
+console.log(combinando)
+
+//metodo split para separar por palavras
+const frase= 'Olá mundo  estou separando por espaços'
+const result= frase.split(' ')
+console.log(result)
+
+//PARA URL DE SITE ,ISLUG
+console.log(result.join('-'))
