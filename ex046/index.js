a5c161f2abbf64130c9f30730bd6316618dad717
gustavoesposito <espositogustavo796@gmail.com>
2022-06-04 10:47:22 -0300

    att

---
+++ ex046/index.js
@@ -0,0 +1,13 @@
+//OPERADOR SPREAD
+const primeiro =[1,2,3]
+const segundo =[4,5,6]
+
+const combinando =[...primeiro,...segundo]
+console.log(combinando)
+//CLONANDO
+const clonado =[...combinando]
+//EXERCICITANDO
+const pri=[1,2,3, '%']
+const seg =[4,5,6]
+const combi =[...pri,...seg]
+console.log(combi)
